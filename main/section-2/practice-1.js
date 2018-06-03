865ed12a61ba3937005c0b61ad6ccd9fbf855281
Leelee Tian <tianlihang2013@gmail.com>
2018-06-03 22:25:52 +0800

    Finish section-2/practice-1

--- main/section-2/practice-1.js
+++ main/section-2/practice-1.js
@@ -1,5 +1,19 @@
 'use strict';
 
 module.exports = function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
+  let statistic = new Array();
+  for(var element of collection){
+      let flag = 1;
+      if(statistic.length !== 0){
+          for(var obj of statistic){
+              if(obj.key === element){
+                  obj.count++;
+                  flag = 0;
+                  break;
+              }
+          }
+      }
+      (flag)? statistic.push({key: element, count: 1}) : null;
+  }
+  return statistic;
 }
