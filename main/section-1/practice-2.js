e1ca666d6653f0ad626bf613aad8a073623e1658
Leelee Tian <tianlihang2013@gmail.com>
2018-06-03 21:06:29 +0800

    Finish section-1/practice-2

--- main/section-1/practice-2.js
+++ main/section-1/practice-2.js
@@ -1,5 +1,12 @@
 'use strict';
 
 module.exports = function collectSameElements(collectionA, collectionB) {
-  return '实现练习要求，并改写该行代码。';
+  let intersection = new Array();
+  for(let i = 0; i < collectionA.length; i++){
+    for(let j = 0; j < collectionB[0].length; j++){
+      if(collectionA[i] === collectionB[0][j])
+        intersection.push(collectionB[0][j]);
+    }
+  }
+  return intersection; 
 }
