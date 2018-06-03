ec99f0b247b1b9e9ce95dd890c748201e81b15ec
Leelee Tian <tianlihang2013@gmail.com>
2018-06-03 23:22:27 +0800

    Finish section-3/practice-4

--- main/section-3/practice-4.js
+++ main/section-3/practice-4.js
@@ -1,5 +1,39 @@
 'use strict';
 
 module.exports = function createUpdatedCollection(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+  collectionA = countSameElements(collectionA);
+  return findSameAndMinus(collectionA, objectB);
+}
+
+function findSameAndMinus(collectionA, objectB) {
+  for(var e of objectB.value){
+    for(var obj of collectionA){
+      if(e === obj.key){
+        let count = Math.floor(obj.count / 3);
+        obj.count -= count;
+      }
+    }
+  }
+  return collectionA;
+}
+
+function countSameElements(collection) {
+  let statistic = new Array();
+  for(var element of collection){
+      let [flag, count] = [1, 1];
+      element = element.split('-');
+      (element.length !== 1)? count = parseInt(element[1]): null;
+      element = element[0];
+      if(statistic.length !== 0){
+          for(var obj of statistic){
+              if(obj.key === element){
+                  obj.count += count;
+                  flag = 0;
+                  break;
+              }
+          }
+      }
+      (flag)? statistic.push({key: element, count: count}) : null;
+  }
+  return statistic;
 }
