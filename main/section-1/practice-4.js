40823cca814bae1ef16fa1e43a95a84e4994b8cd
Leelee Tian <tianlihang2013@gmail.com>
2018-06-03 22:34:55 +0800

    use for...of in section-1/practice-4

--- main/section-1/practice-4.js
+++ main/section-1/practice-4.js
@@ -2,10 +2,10 @@
 
 module.exports = function collectSameElements(collectionA, objectB) {
   let intersection = new Array();
-  for(let i = 0; i < collectionA.length; i++){
-    for(let j = 0; j < objectB.value.length; j++){
-      if(collectionA[i].key === objectB.value[j])
-        intersection.push(collectionA[i].key);
+  for(var obj of collectionA){
+    for(var e of objectB.value){
+      if(obj.key === e)
+        intersection.push(e);
     }
   }
   return intersection;
